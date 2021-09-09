3fbf838bd7263088f47b3e2360f5f700e609989d
Jules laurens Van der hoek <juvan-de@f0r4s2.codam.nl>
2021-09-09 18:31:50 +0200

    smal optimiliziationeniion

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: julesvanderhoek <julesvanderhoek@studen      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/07/06 17:33:08 by julesvander   #+#    #+#                 */
-/*   Updated: 2021/09/09 18:23:20 by juvan-de      ########   odam.nl         */
+/*   Updated: 2021/09/09 18:25:26 by juvan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	goto_exit(t_philo *philo, pthread_t *threads)
 
 int	main(int argc, char **argv)
 {
-	int			i;
 	t_data		data;
 	pthread_t	*threads;
 	t_philo		*philos;
@@ -32,8 +31,7 @@ int	main(int argc, char **argv)
 		printf("Incorrect arguments\n");
 		return (0);
 	}
-	i = data_init(argv, &data);
-	if (i == 1 || data.philo_num <= 1)
+	if (data_init(argv, &data) == 1)
 	{
 		printf("Invalid input\n");
 		return (0);
